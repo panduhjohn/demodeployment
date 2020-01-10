5b33ceb05e821384b1df40988e46f000e76501a8
John Stilwell <johnrstilwell@codeimmersives.com>
2020-01-10 01:50:16 -0500

    edit weather statement

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, 'The weather is ' + body.daily.data[0].summary.toLowerCase() + ' With a temp of ' + body.currently.temperature + ' and a ' + body.currently.precipProbability + '% chance of rain.');
+            callback(undefined, 'It is currently ' + body.daily.data[0].summary.toLowerCase() + ' With a temperature of ' + body.currently.temperature + '. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.');
         }
     });
 };
